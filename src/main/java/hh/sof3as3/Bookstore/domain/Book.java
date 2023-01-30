f1602116c22feb7120895378d4a8ab37d5a31526
Ada Meriläinen <ada.merilainen@gmail.com>
2023-01-30 15:19:55 +0200

    added Book.java and updated Controller

--- src/main/java/hh/sof3as3/Bookstore/domain/Book.java
+++ src/main/java/hh/sof3as3/Bookstore/domain/Book.java
@@ -6,4 +6,71 @@ public class Book {
 	 public Integer year;
 	 public String isbn;
 	 public double price;
+	 
+	public Book(String title, String author, Integer year, String isbn, double price) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.isbn = isbn;
+		this.price = price;
+	}
+
+	public Book() {
+		super();
+		this.title = null;
+		this.author = null;
+		this.year = null;
+		this.isbn = null;
+		this.price = 0.00;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn + ", price=" + price;
+	}
+	
+	
+	
+	 
 }
--- src/main/java/hh/sof3as3/Bookstore/webcontroller/BookController.java
+++ src/main/java/hh/sof3as3/Bookstore/webcontroller/BookController.java
@@ -10,6 +10,8 @@ public class BookController {
 	@GetMapping("/index")
 	public String addBook() {
 		
+		return "bookWelcome";
+		
 	}
 
 }
