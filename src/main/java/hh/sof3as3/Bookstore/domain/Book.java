70d85c969e585b42a12d5ae7ad75e7c4b45e85dc
Ada Meriläinen <ada.merilainen@gmail.com>
2023-01-30 13:11:36 +0200

    first version of the project

---
+++ src/main/java/hh/sof3as3/Bookstore/domain/Book.java
@@ -0,0 +1,9 @@
+package hh.sof3as3.Bookstore.domain;
+
+public class Book {
+	 public String title;
+	 public String author;
+	 public Integer year;
+	 public String isbn;
+	 public double price;
+}
---
+++ src/main/java/hh/sof3as3/Bookstore/webcontroller/BookController.java
@@ -0,0 +1,15 @@
+package hh.sof3as3.Bookstore.webcontroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+
+	
+	@GetMapping("/index")
+	public String addBook() {
+		
+	}
+
+}
