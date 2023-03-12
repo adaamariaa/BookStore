7248fd2aa809582c239a6932d73d152020efbc13
Ada Meriläinen <ada.merilainen@gmail.com>
2023-03-12 11:54:30 +0200

    Added properties

--- src/main/java/hh/sof3as3/Bookstore/BookstoreApplication.java
+++ src/main/java/hh/sof3as3/Bookstore/BookstoreApplication.java
@@ -1,21 +1,21 @@
 package hh.sof3as3.Bookstore;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Bean;
 
-import hh.sof3as3.Bookstore.domain.Book;
-import hh.sof3as3.Bookstore.domain.BookRepository;
-import hh.sof3as3.Bookstore.domain.Category;
-import hh.sof3as3.Bookstore.domain.CategoryRepository;
+//import hh.sof3as3.Bookstore.domain.Book;
+//import hh.sof3as3.Bookstore.domain.BookRepository;
+//import hh.sof3as3.Bookstore.domain.Category;
+//import hh.sof3as3.Bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
 	
-	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class); 
+	//private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class); 
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
