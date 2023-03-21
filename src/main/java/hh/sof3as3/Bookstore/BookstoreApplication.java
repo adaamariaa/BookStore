075281285d02f0c98b6c8149be922befe0a68f2b
Ada Meriläinen <ada.merilainen@gmail.com>
2023-03-21 19:19:47 +0200

    fixed some texts

--- src/main/java/hh/sof3as3/Bookstore/BookstoreApplication.java
+++ src/main/java/hh/sof3as3/Bookstore/BookstoreApplication.java
@@ -3,8 +3,8 @@ package hh.sof3as3.Bookstore;
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
 //import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.context.annotation.Bean;
 
 //import hh.sof3as3.Bookstore.domain.Book;
@@ -12,13 +12,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import hh.sof3as3.Bookstore.domain.Category;
 //import hh.sof3as3.Bookstore.domain.CategoryRepository;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class BookstoreApplication {
 	
 	//private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class); 
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookstoreApplication.class, args);
+		//SpringApplication.run(BookstoreApplication.class, args);
 	}
 
 	
